d1169180c4c856f00b9a5b81f74e11644b389e30
js.park1021 <js.park1021@ptn.tmoney.co.kr>
2024-11-05 13:40:54 +0900

    1105

--- Section4-MonsterGame/app.js
+++ Section4-MonsterGame/app.js
@@ -1,16 +1,35 @@
 function getRandomValue(min, max) {
-  return Math.floor(Math.random() * (min - max)) + 5;
+  return Math.floor(Math.random() * (min - max)) + min;
 }
+
+let currentRound = 0;
 
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
+  },
+  computed: {
+    monsterBarStyles() {
+      return {
+        width: this.monsterHealth + "%",
+      };
+    },
+    playerBarStyles() {
+      return {
+        width: this.playerHealth + "%",
+      };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlyer();
@@ -18,6 +37,12 @@ const app = Vue.createApp({
     attackPlyer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlyer();
     },
   },
 });
