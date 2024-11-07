f61a6ebd47ccfba78d9fab0007f6ec6cf80ac7cb
js.park1021 <js.park1021@ptn.tmoney.co.kr>
2024-11-07 10:35:35 +0900

    1107

--- Section4-MonsterGame/app.js
+++ Section4-MonsterGame/app.js
@@ -15,11 +15,17 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return {
         width: this.monsterHealth + "%",
       };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return {
         width: this.playerHealth + "%",
       };
@@ -46,6 +52,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.urrentRound = 0;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
@@ -71,6 +83,9 @@ const app = Vue.createApp({
         this.playerHealth += healValue;
       }
       this.attackPlyer();
+    },
+    surrender() {
+      this.winner = "monster";
     },
   },
 });
