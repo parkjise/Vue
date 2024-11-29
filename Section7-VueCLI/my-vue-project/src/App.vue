28c81b392660b7861c7ec0ab581812f3596fce8d
js.park1021 <js.park1021@ptn.tmoney.co.kr>
2024-11-29 18:15:11 +0900

    1129

---
+++ Section7-VueCLI/my-vue-project/src/App.vue
@@ -0,0 +1,129 @@
+<template>
+  <section>
+    <header>
+      <h2>My Friends</h2>
+    </header>
+    <ul>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-Favorite="toggleFavoriteStatus"
+      ></friend-contact>
+    </ul>
+  </section>
+
+  <main></main>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@gmail.com",
+          isFavorite: true,
+        },
+        {
+          id: "julie",
+          name: "julie Lorenz",
+          phone: "0123 45678 90",
+          email: "julie@gmail.com",
+          isFavorite: false,
+        },
+        {
+          id: "JS",
+          name: "JiSe Park",
+          phone: "0123 45678 90",
+          email: "JS@gmail.com",
+          isFavorite: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(
+        (friend) => friend.id === friendId
+      );
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+  },
+};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+header h2 {
+  color: #fff;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ Section7-VueCLI/my-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,84 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }}
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone : </strong>{{ phoneNumber }}</li>
+      <li><strong>Email : </strong>{{ emailAddress }}</li>
+      <li>1</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function (value) {
+      //   return value === "1" || value === "0";
+      // },
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      // this.firendsIsFavorite = !this.firendsIsFavorite;
+      this.$emit("toggle-Favorite", this.id);
+    },
+  },
+};
+</script>
+
+<!-- 
+일반적으로 다음 공식 문서에서 프로퍼티 유효성 검사에 대한 모든 것을 
+배울 수 있습니다. https://v3.vuejs.org/guide/component-props.html
+
+특히 다음 값 타입(type  프로퍼티)이 지원됩니다.
+
+문자열(String)
+
+숫자(Number)
+
+불리언(Boolean)
+
+배열(Array)
+
+객체(Object)
+
+날짜(Date)
+
+함수(Function)
+
+심볼(Symbol)
+
+생성자 함수(Date와 같은 내장된 함수 또는 커스텀 함수)도 타입이 될 수 있습니다.
+-->
