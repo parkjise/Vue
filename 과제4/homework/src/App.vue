d7a2da18a650af4a9434b957162e70a6af1643cd
parkjise <monories79@gmail.com>
2024-12-15 14:58:08 +0900

    1215

---
+++ 과제4/homework/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<active-user :username="user.name" :userage="user.age"></active-user>
+		<user-data @set-data="setUserData"></user-data>
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			user: {
+				name: "Park Ji Se",
+				age: 44,
+			},
+		};
+	},
+	methods: {
+		setUserData(name, age) {
+			this.user = {
+				name: name,
+				age: +age,
+			};
+		},
+	},
+};
+</script>
+<style scoped></style>
