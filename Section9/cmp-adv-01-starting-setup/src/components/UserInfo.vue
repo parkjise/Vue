a140b48b8914aa3ae40bb381a544f61e58946ea9
parkjise <monories79@gmail.com>
2025-01-05 10:57:46 +0900

    0104

--- Section9/cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ Section9/cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,39 +1,37 @@
 <template>
-  <section>
-    <base-card>
-      <template v-slot:header>
-        <header>
-          <h3>{{ fullName }}</h3>
-          <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-        </header>
-      </template>
-      <template v-slot:default>
-        <p>{{ infoText }}</p>
-      </template>
-    </base-card>
-  </section>
+	<section>
+		<base-card>
+			<template #header>
+				<h3>{{ fullName }}</h3>
+				<base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+			</template>
+			<template #default>
+				<p>{{ infoText }}</p>
+			</template>
+		</base-card>
+	</section>
 </template>
 
 <script>
-import BaseCard from './BaseCard.vue';
+import BaseCard from "./BaseCard.vue";
 export default {
-  components: { BaseCard },
-  props: ['fullName', 'infoText', 'role'],
+	components: { BaseCard },
+	props: ["fullName", "infoText", "role"],
 };
 </script>
 
 <style>
-section {
+/* section {
   margin: 2rem auto;
   max-width: 30rem;
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
-}
+} */
 
 /* section div {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 } */
 </style>
